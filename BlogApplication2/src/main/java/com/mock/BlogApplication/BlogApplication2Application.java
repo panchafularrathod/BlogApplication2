22b5988099474503559f45b6e6feb152f300b7da
panchafula <panchafularathod1998.com>
2024-05-27 12:22:46 +0100

    add project on github

---
+++ BlogApplication2/src/main/java/com/mock/BlogApplication/BlogApplication2Application.java
@@ -0,0 +1,15 @@
+package com.mock.BlogApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication2Application.class, args);
+		
+		System.out.println("this is stating..............");
+	}
+
+}
---
+++ BlogApplication2/src/test/java/com/mock/BlogApplication/BlogApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock.BlogApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
